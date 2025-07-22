b91c58abecf786ccb2012208f1d10561ba24c8b1
Lucas215zz <153696538+Lucas215zz@users.noreply.github.com>
2025-07-22 18:49:01 +0800

    v3.3
    修复bug

--- themes/theme.js
+++ themes/theme.js
@@ -1,6 +1,7 @@
 // 只保留Muzli主题的导出，修复@theme-components报错
 import MuzliHome from './muzli'
 
+// 主题组件导出
 export const THEME_CONFIG = {
   LayoutBase: MuzliHome,
   LayoutIndex: MuzliHome,
@@ -14,9 +15,25 @@ export const THEME_CONFIG = {
 }
 
 export const getThemeConfig = async () => THEME_CONFIG
-
 export const getBaseLayoutByTheme = () => MuzliHome
-
 export const DynamicLayout = props => <MuzliHome {...props} />
-
 export const getLayoutByTheme = () => MuzliHome
+
+// 兼容全局调用的暗色模式相关函数
+export function initDarkMode(updateDarkMode, defaultDarkMode) {
+  if (typeof updateDarkMode === 'function') {
+    updateDarkMode(false)
+  }
+  if (typeof document !== 'undefined') {
+    document.getElementsByTagName('html')[0].setAttribute('class', 'light')
+  }
+}
+export function isPreferDark() {
+  return false
+}
+export function loadDarkModeFromLocalStorage() {
+  return null
+}
+export function saveDarkModeToLocalStorage(newTheme) {
+  // do nothing
+}
